9c4bda9628e478dd48a53f9497bb57ba682f39f8
L3on06 <l3onshabani@gmail.com>
2022-11-20 21:05:55 -0800

    I added the virtual page.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header from './Components/Header/Header';
 import Hero from './Components/Hero/Hero';
 import Slider from './Components/Slider/Slider';
+import Virtual from './Components/Virtual/Virtual';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Header />
       <Hero />
       <Slider />
+      <Virtual />
     </div>
   );
 }
