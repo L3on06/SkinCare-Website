001688dfff570234eb12f18868e339350e66f23b
L3on06 <l3onshabani@gmail.com>
2022-11-27 21:42:37 -0800

    I added some animation on hero page.

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -3,22 +3,47 @@ import css from './Hero.module.css'
 import HeroImg from "../../assets/hero.png";
 import { RiShoppingBagFill } from "react-icons/ri"
 import { BsArrowRight } from "react-icons/bs"
+import { motion } from "framer-motion"
 
 const Hero = () => {
+
+    const transition = { duration: 3, type: "spring" }
+
     return (
         <div className={css.container}>
             <div className={css.h_sliders}>
-                <span className={css.text1}>Skin Protect Care</span>
-                <div className={css.text2}>
+                <motion.span
+                    // initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.8, delay: 1 }}
+                    className={css.text1}>Skin Protect Care</motion.span>
+                <motion.div
+                    // initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.8, delay: 1.2 }}
+                    className={css.text2}>
                     <span>Trendy Collection</span>
                     <span>Blanditiis voluptatum commodi officiis magni optio, pariatur repellendus!</span>
-                </div>
+                </motion.div>
             </div>
 
             <div className={css.wrapper}>
-                <div className={css.blueCircle}></div>
-                <img src={HeroImg} alt="Hero Img" width={600} />
-                <div className={css.cart2}>
+                <motion.div
+                    initial={{ bottom: "5rem" }}
+                    whileInView={{ bottom: "0rem" }}
+                    transition={transition}
+                    className={css.blueCircle}>
+                </motion.div>
+                <motion.img
+                    initial={{ bottom: "-5rem" }}
+                    whileInView={{ bottom: "0rem" }}
+                    transition={transition}
+                    src={HeroImg} alt="Hero Img" width={600} />
+                <motion.div
+                    initial={{ right: "14%" }}
+                    whileInView={{ right: "0%" }}
+                    transition={transition}
+                    className={css.cart2}>
                     <RiShoppingBagFill />
                     <div className={css.signup}>
                         <span>Best Sign up Offers</span>
@@ -26,20 +51,28 @@ const Hero = () => {
                             <BsArrowRight />
                         </div>
                     </div>
-                </div>
+                </motion.div>
             </div>
 
 
             <div className={css.h_sliders}>
-                <div className={css.traffic}>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.8, delay: 1.4 }}
+                    className={css.traffic}>
                     <span>1.5m</span>
                     <span>Monthy Traffic</span>
-                </div>
+                </motion.div>
 
-                <div className={css.customers}>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    whileInView={{ opacity: 1 }}
+                    transition={{ duration: 0.8, delay: 1.6 }}
+                    className={css.customers}>
                     <span>200k</span>
                     <span>Happy Customers</span>
-                </div>
+                </motion.div>
             </div>
         </div>
     )
